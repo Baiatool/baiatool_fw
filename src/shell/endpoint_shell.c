5ab139d6add5fb3cc8e7cd10372b0c8e0df0b0c9
Jose Felix <jfon@ic.ufal.br>
2026-07-01 11:14:59 -0300

    feat: add shell command for POST checkout

--- src/shell/endpoint_shell.c
+++ src/shell/endpoint_shell.c
@@ -20,6 +20,7 @@
 
 #if CONFIG_SHELL_ENDPOINT
 
+#include "endpoints/checkout.h"
 #include "endpoints/confirm_schedule.h"
 #include "endpoints/schedule.h"
 
@@ -114,11 +115,38 @@ static int cmd_confirm_schedule(const struct shell *sh, size_t argc, char **argv
 	return 0;
 }
 
+static int cmd_checkout(const struct shell *sh, size_t argc, char **argv)
+{
+	const char *host = argv[1];
+	const char *port = argv[2];
+	int sock;
+	int ret;
+
+	sock = connect_to_host(sh, host, port);
+	if (sock < 0) {
+		return sock;
+	}
+
+	ret = checkout_endpoint_post(sock, host, port);
+
+	(void)zsock_close(sock);
+
+	if (ret < 0) {
+		shell_error(sh, "checkout failed: %d", ret);
+		return ret;
+	}
+
+	shell_print(sh, "checkout OK");
+	return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(endpoint_cmds,
 			       SHELL_CMD_ARG(fetch_schedule, NULL, "<host> <port>",
 					     cmd_fetch_schedule, 3, 0),
 			       SHELL_CMD_ARG(confirm_schedule, NULL, "<host> <port>",
 					     cmd_confirm_schedule, 3, 0),
+			       SHELL_CMD_ARG(checkout, NULL, "<host> <port>",
+					     cmd_checkout, 3, 0),
 			       SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(endpoint, &endpoint_cmds, "Endpoint commands", NULL);
